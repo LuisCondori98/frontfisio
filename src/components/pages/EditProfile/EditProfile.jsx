aefd482d22a4bf473a57438d15fb0ab0ceb6c8ca
unknown <lcondori11@hotmail.com>
2026-01-29 17:56:24 -0500

    practice route

--- src/components/pages/EditProfile/EditProfile.jsx
+++ src/components/pages/EditProfile/EditProfile.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 const EditProfile = () => {
