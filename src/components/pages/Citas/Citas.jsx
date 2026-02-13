8fe63d4e4196770ccbd2083ca5b953dc7ccbb7c5
unknown <lcondori11@hotmail.com>
2026-02-12 20:44:44 -0500

    css bootstrap

--- src/components/pages/Citas/Citas.jsx
+++ src/components/pages/Citas/Citas.jsx
@@ -50,7 +50,7 @@ const Citas = () => {
       {
         user.rol === "admin"?
         (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
             {citas.map((c) => (
               <div key={c._id} className="bg-white rounded-xl shadow-md overflow-hidden flex flex-col h-full">
                 <div className="p-4 flex-1">
