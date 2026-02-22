ece64b345d614259df40757062151e3c1f67e8c3
unknown <lcondori11@hotmail.com>
2026-02-22 18:16:06 -0500

    add img resize

--- src/components/pages/Index/Index.jsx
+++ src/components/pages/Index/Index.jsx
@@ -144,6 +144,7 @@ const Index = () => {
                   src={`/img/${f.img}`}
                   className="card-img-top"
                   alt={f.nombre}
+                  style={{ height: "450px", objectFit: "cover" }}
                 />
                 <div className="card-body">
                   <h5 className="card-title fw-bold">
