e935bc2a0ea5ff206ab54dfb1b6dacb8294357fb
unknown <lcondori11@hotmail.com>
2026-02-25 10:00:44 -0500

    lastname add

--- src/components/pages/Index/Index.jsx
+++ src/components/pages/Index/Index.jsx
@@ -157,7 +157,7 @@ const Index = () => {
                     />
                     <div className="card-body">
                       <h5 className="card-title fw-bold">
-                        {(f.nombre).toLowerCase().split(" ").charAt(0).toUpperCase()}
+                        {f.nombre} {f.apellidoPaterno}
                       </h5>
                       <p className="card-text text-muted">
                         Fisioterapeuta
