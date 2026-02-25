d24fe95ae99a58c0a157ccfe4af1d82944157161
unknown <lcondori11@hotmail.com>
2026-02-24 19:04:24 -0500

    add sweet alert generate cita 7

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -175,7 +175,7 @@ const Profile = () => {
                   <div className="card-body">
                         {
                           cita.map(c => (
-                            c.estado === "pendiente"?
+                            c.estado !== "completada"?
                             <div className="row align-items-center">
                               <div className="col-md-8">
                               <h4 className="text-success mb-3">Terapia de Rehabilitación</h4>
